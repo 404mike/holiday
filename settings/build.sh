cba02aa03410a0c31ae85180339531376faf2ee5
Mike <michaeljones401@gmail.com>
2014-10-22 14:32:30 +0100

    build script

---
+++ settings/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Build script to move conf files and build laravel
+
+# Move apache conf file
+echo "Moving apache conf file"
+sudo cp /var/www/settings/apache2.conf /etc/apache2/apache2.conf
+
+# Move site available directory
+echo "Moving sites-available directory"
+sudo cp -R /var/www/settings/sites-available /etc/apache2/
+
+# Move site available directory
+echo "Moving php.ini file"
+sudo cp /var/www/settings/php.ini /etc/php5/cli/
+
+# Move site available directory
+echo "Moving mongodb conf file"
+sudo cp /var/www/settings/mongodb.conf /etc/
+
+# Build laravel
+echo "Building laravel"
+echo "Moving into /var/www/"
+cd /var/www/
+echo "Downloading composer"
+curl -sS https://getcomposer.org/installer | php
+echo "Building laravel"
+php composer.phar install
+echo "Migrarting database"
+php artisan migrate
+echo "Seeding the database"
+php artisan db:seed
