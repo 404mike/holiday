67abab08be26816e338b0c5ffb8b7e9148e08dbe
Mike Jones <michaeljones401@gmail.com>
2014-11-26 00:17:27 +0000

    interface update

--- app/views/story/main.php
+++ app/views/story/main.php
@@ -70,25 +70,30 @@
 
 <iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=<?php echo $story['mainLoc']; ?>&key=AIzaSyBvF5X5MSWWR5C7QxB96PkcztsJua-OMTY"></iframe>
 
+
 <?php
-// echo '<pre>' , print_r($story['mainLoc']) , '</pre>';
 
 $date = '';
 $dayCount = 1;
+
+echo '<div class="row">';
 
 foreach($story['items'] as $s => $value)
 {
 	$v = $value[$s+1];
 
-	if(isset($v['created_at'])) {
-		// echo '<h2>' . date('Y-m-d' , $v['created_at']) . '</h2>';
+	if(isset($v['created_at'])) 
+	{
 		$itemDate = date('Y-m-d' , $v['created_at']);
 
 		if($date == '') {
+			echo getItemInfo($v);
 			$date = $itemDate;
 		}
 		elseif($date < $itemDate) {
 			$date = $itemDate;
+			echo '</div>';
+
 			echo '<div class="story_date">';
 			echo '<h2>Day ' . $dayCount . ' - ' . date('l dS F Y', strtotime($date)) . '</h2>';
 			
@@ -103,34 +108,92 @@ foreach($story['items'] as $s => $value)
 			echo '<i class="'.$weather[$weatherType].'"></i>';
 			echo '<span class="weather_temp">'.$weatherShort['tempC'] . '</span><i class="wi wi-celsius"></i>';
 			echo '</div>';
+
+
+			echo '<div class="row">';
+			echo getItemInfo($v);
+
 		}
-	}
-
-	if( $v['display'] != 'false' ) { //
-
-		if($v['type'] == 'text' && $v['message'] == '') continue;
-
- 		echo '<div class="story_item">';
-
-		if(array_key_exists('type', $v)) {
-			echo '<h2>Type '.$v['type'].'</h2>';
+		else{
+			echo getItemInfo($v);
 		}
-
-		if(array_key_exists('message', $v)) {
-			echo '<p>' . $v['message'] . '</p>';
-		}
-
-		if(array_key_exists('picture', $v)) {
-
-			if(preg_match('/http(.?)/', $v['picture'])) {
-				echo '<img width=200 src="' . $v['picture'] .'" />';
-			}else {
-				echo '<img width=200 src="/photos/' . $v['picture'] .'" />';
-			}
-		}
-		
-		echo '</div>';
+	}else{
+		echo getItemInfo($v);
 	}
 }
 
-?>
+echo '</div>';
+
+function getItemInfo( $v )
+{
+	if($v['display'] == 'false') {
+		return;
+	}
+	if(isset($v['message'])) {
+		if($v['message'] == '' ){
+			return;
+		}		
+	}
+
+	$result = 
+	' 
+	<div class="item">
+		<div class="well">
+	';
+
+	$type = $v['type'];
+	if($type == 'image') {
+		$result .= getImageData( $v );
+	}
+	elseif($type == 'facebookfeed') {
+		$result .= getFacebookData( $v );
+	}
+	elseif($type == 'tweet') {
+		$result .= getTweetData( $v );
+	}
+	elseif($type == 'text') {
+		$result .= getTextData( $v );
+	}else{
+		return '';
+	}
+
+	$result .= '
+		</div>
+	</div>';
+
+	return $result;
+}
+
+
+function getImageData( $v )
+{
+	if(preg_match('/http(.?)/', $v['picture'])) {
+		$img = '<img width=200 src="' . $v['picture'] .'" />';
+	}else {
+		$img = '<img width=200 src="/photos/' . $v['picture'] .'" />';
+	}
+
+	return $img;
+}
+
+function getFacebookData( $v )
+{
+	return $v['message'];
+}
+
+function getTweetData( $v )
+{
+	return $v['message'];
+}
+
+function getTextData( $v )
+{
+	return $v['message'];
+}
+
+function getDayWeather( $v )
+{
+
+}
+
+
